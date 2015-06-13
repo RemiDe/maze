98e638d01170fd3d136a202d517b9057a5bfcbca
Ryu I <ryui@ryu-i-2.local>
2015-06-13 21:11:27 +0900

    add stage

--- maze/ThirdViewController.swift
+++ maze/ThirdViewController.swift
@@ -10,27 +10,39 @@ import UIKit
 import CoreMotion
 
 class ThirdViewController: UIViewController {
+    let myBoundSize: CGSize = UIScreen.mainScreen().bounds.size
     
     var motionManager: CMMotionManager!
+    var imageView: UIImageView!
     var speedX: Double = 0.0
     var speedY: Double = 0.0
-    private var dot: UIImageView!
+    var posX: CGFloat = 0.0
+    var posY: CGFloat = 0.0
     
-    @IBOutlet var overView: UIView!
-
+    @IBOutlet var overView1: UIImageView!
+    @IBOutlet var overView2: UIImageView!
+    @IBOutlet var overView3: UIImageView!
+    @IBOutlet var overView4: UIImageView!
+    @IBOutlet var clearView: UIImageView!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var backBt: UIButton!
+    
+    @IBAction func back(){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // UIImageViewを作成する.
-        dot = UIImageView(frame: CGRectMake(0,0,30,30))
-        // 表示する画像を設定する.
-        let myImage = UIImage(named: "dot_green.png")
-        // 画像をUIImageViewに設定する.
-        dot.image = myImage
-        // 画像の表示する座標を指定する.
-        dot.layer.position = CGPoint(x: self.view.bounds.width/2, y: 500.0)
-        // UIImageViewをViewに追加する.
-        self.view.addSubview(dot)
+        statusLabel.text = ""
+        backBt.enabled = false
+        backBt.hidden = true
+        
+        var image = UIImage(named: "dot_green")!
+        imageView = UIImageView(frame: CGRect(x: self.view.center.x, y: 500, width: 30, height: 30))
+        imageView.image = image
+        
+        self.view.addSubview(imageView)
         
         //MotionManagerを生成
         motionManager = CMMotionManager()
@@ -42,46 +54,100 @@ class ThirdViewController: UIViewController {
         self.startMotionManager()
     }
     
-        func startMotionManager() {
-            //加速度の取得を開始
-            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue(), withHandler:
-                {(accelerometerData:CMAccelerometerData!, error:NSError!) -> Void in
+    func startMotionManager() {
+        //加速度の取得を開始
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler:
+            {(accelerometerData:CMAccelerometerData!, error:NSError!) -> Void in
+                if (self.posX < 0 || self.posX > self.myBoundSize.width) {
                     
-                self.speedX += accelerometerData.acceleration.x
-                self.speedY += accelerometerData.acceleration.y
+                    self.speedX = (-1)*self.speedX
                     
-                var posX = self.dot.center.x + CGFloat(self.speedX)
-                var posY = self.dot.center.y + CGFloat(self.speedY)
+                }else if self.posY < 0 || self.posY > self.myBoundSize.height  {
                     
-                self.dot.center = CGPointMake(posX, posY)
-                //self.checkGameOver()
-            })
-        }
+                    self.speedY = (-1)*self.speedY
+                    
+                } else {
+                    
+                    self.speedX += accelerometerData.acceleration.x
+                    self.speedY += accelerometerData.acceleration.y
+                }
+                self.posX = self.imageView.center.x + CGFloat(self.speedX)
+                self.posY = self.imageView.center.y - CGFloat(self.speedY)
+                //NSLog("posX : %f",posX)
+                //NSLog("posY : %f",posY)
+                
+                self.imageView.center = CGPointMake(self.posX, self.posY)
+                self.gameOver()
+                self.clear()
+                
+        })
+    }
     
     //ふれたら終わるやつ
-    func gameOver(){
-        if CGRectContainsPoint(overView.frame, dot.center){
-            if (motionManager.accelerometerActive){
+    func gameOver() {
+        if CGRectContainsPoint(overView1.frame, imageView.center) {
+            statusLabel.text = "Game Over!"
+            backBt.enabled = true
+            backBt.hidden = false
+            if (motionManager.accelerometerActive) {
+                motionManager.stopAccelerometerUpdates()
+            }
+        }
+        if CGRectContainsPoint(overView2.frame, imageView.center) {
+            statusLabel.text = "Game Over!"
+            backBt.enabled = true
+            backBt.hidden = false
+            if (motionManager.accelerometerActive) {
+                motionManager.stopAccelerometerUpdates()
+            }
+        }
+        if CGRectContainsPoint(overView3.frame, imageView.center) {
+            statusLabel.text = "Game Over!"
+            backBt.enabled = true
+            backBt.hidden = false
+            if (motionManager.accelerometerActive) {
+                motionManager.stopAccelerometerUpdates()
+            }
+        }
+        if CGRectContainsPoint(overView4.frame, imageView.center) {
+            statusLabel.text = "Game Over!"
+            backBt.enabled = true
+            backBt.hidden = false
+            if (motionManager.accelerometerActive) {
                 motionManager.stopAccelerometerUpdates()
             }
         }
     }
     
-            // Do any additional setup after loading the view.
+    //ふれたらクリア
+    func clear() {
+        if CGRectContainsPoint(clearView.frame, imageView.center) {
+            statusLabel.text = "Clear!"
+            statusLabel.textColor = UIColor.blueColor()
+            backBt.enabled = true
+            backBt.hidden = false
+            if (motionManager.accelerometerActive) {
+                motionManager.stopAccelerometerUpdates()
+            }
+            
+        }
+    }
+    
+    // Do any additional setup after loading the view.
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
